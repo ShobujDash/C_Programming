c8eab3fd7855d55e563333d582b470f3667a20cc
ShobujDas <shobujd6.gmail.com>
2025-05-17 10:27:41 +0600

    added chapter 02 new things

--- Chapter02/Hello.c
+++ Chapter02/Hello.c
@@ -1,8 +1,40 @@
 # include<stdio.h>
+# include<math.h>
 
 int main(){
 
-  printf("hello world \n");
+  // printf("hello world \n");
+
+  //  Arithmetic Instructions
+  // int a = 1, b = 2;
+  // int sum = a + b;
+  // int multiply = a * b;
+  // int c = 2;
+  // int power = pow(b, c);
+
+  // printf("%d", power);
+
+  int x;
+  printf("enter a number : ");
+  scanf("%d", &x);
+  printf("%d", x % 2 == 0);
 
   return 0;
 }
+
+//  Instructions
+// control Instructions
+// Used to determine flow of program
+// a. Sequence Control
+// b. Decision Control 
+// c. Loop Control 
+// d. Case Control
+
+
+//  Operators
+// a. Arithmetic Operators (+,-,* ,/,%)
+// b. Relations Operators (== , < ,<= , > , >=, !=)
+// c. Logical Operators (|| , && , ! )
+// d. Bitwise Operators
+// e. Assignment Operators (= , +=, -=, *= , /=, %=)
+// f. Ternary Operators
---
+++ Chapter02/conditional.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+// Conditional Statements 
+// 1. if else 
+// 2. switch 
+
+
+int main(){
+
+  return 0;
+}
